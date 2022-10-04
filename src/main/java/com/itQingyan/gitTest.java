8edfbfaae7c90e1b4d49b549ffdf43a26dfeba12
RuiQingyan <Wei2836377438@163.com>
2022-10-04 10:18:49 +0800

    second commit

--- src/main/java/com/itQingyan/gitTest.java
+++ src/main/java/com/itQingyan/gitTest.java
@@ -3,5 +3,7 @@ package com.itQingyan;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git!222222");
     }
 }
